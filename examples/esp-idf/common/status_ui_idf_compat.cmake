0aa44483a7929d714492a51ebe3787a97f5d1f13
Y1hsiaochunnn <y1hsiaochunnn@qq.com>
2026-08-10 13:25:10 +0800

    fix: restore IDF example component dependencies

--- examples/esp-idf/common/status_ui_idf_compat.cmake
+++ examples/esp-idf/common/status_ui_idf_compat.cmake
@@ -1,4 +1,7 @@
 if(IDF_VERSION_MAJOR GREATER_EQUAL 6)
     idf_component_get_property(status_ui_idf_compat_lvgl_lib lvgl__lvgl COMPONENT_LIB)
     target_compile_options(${status_ui_idf_compat_lvgl_lib} PRIVATE -Wno-error=attributes)
+
+    idf_component_get_property(status_ui_idf_compat_status_ui_lib status_ui COMPONENT_LIB)
+    target_compile_options(${status_ui_idf_compat_status_ui_lib} PRIVATE -Wno-error=missing-field-initializers)
 endif()
